7b9f0f8a9d1a4c85cd2f5e6ce7dddce3437a5c61
ayakandoussi <ayakandoussi1905@gmail.com>
2024-12-26 16:49:09 +0100

    classe utilisateur

--- gestion-quiz/src/E_Quiz/domain/Utilisateur.java
+++ gestion-quiz/src/E_Quiz/domain/Utilisateur.java
@@ -2,11 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package E_Quiz.domain;
 
 /**
  *
  * @author User
  */
-public class testest {
+public class Utilisateur {
     
 }
